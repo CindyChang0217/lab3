51c62570fc73351b806108b82c4ef09cf298cab1
CindyChang0217 <102627093+CindyChang0217@users.noreply.github.com>
2022-10-14 13:09:00 -0700

    Lab 3

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testreverseinplace2(){
+    int [] input={50,40,30,20,10};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[] {10,20,30,40,50}, input);
+  }
+
+  @Test
+  public void testreversed2(){
+    int [] input={50,40,20,10};
+    assertArrayEquals(new int[] {10,20,40,50}, ArrayExamples.reversed(input));
+  }
 }
+
